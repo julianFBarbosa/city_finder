509370d9e1bb94f79a3c1c477b1a13bb8ea44c2e
Julian Barbosa <barbosabjf@gmail.com>
2023-11-28 13:56:31 -0300

    add city Controller

---
+++ app/Http/Controllers/CityController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\CityResource;
+use App\Http\Requests\CityRequest;
+use App\Services\CityService;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+
+class CityController extends Controller
+{
+    protected $cityService;
+
+    public function __construct(CityService $cityService)
+    {
+        $this->cityService = $cityService;
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request, $state)
+    {
+        $isStateValid = $this->cityService->validateState($state);
+
+        if(is_null($isStateValid)) {
+            return response()->json(["error" => "O estado inserido não existe"], 400);
+        }
+
+        $cities = $this->cityService->getCityListByState($state);
+        return CityResource::normalizeData($cities);
+    }
+}
